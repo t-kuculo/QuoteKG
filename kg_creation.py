2be974786aceb3b441063b35410a1793ed6eda0c
Simon Gottschalk <simon-gottschalk@kabelmail.de>
2022-03-02 11:39:03 +0100

    Update kg_creation.py
    add comment about date format

--- kg_creation.py
+++ kg_creation.py
@@ -20,7 +20,7 @@ import urllib
 
 all_languages = ["aa","ab","ae","af","ak","am","an","ar","as","av","ay","az","ba","be","bg","bh","bi","bm","bn","bo","br","bs","ca","ce","ch","co","cr","cs","cu","cv","cy","da","de","dv","dz","ee","el","en","eo","es","et","eu","fa","ff","fi","fj","fo","fr","fy","ga","gd","gl","gn","gu","gv","ha","he","hi","ho","hr","ht","hu","hy","hz","ia","id","ie","ig","ii","ik","io","is","it","iu","ja","jv","ka","kg","ki","kj","kk","kl","km","kn","ko","kr","ks","ku","kv","kw","ky","la","lb","lg","li","ln","lo","lt","lu","lv","mg","mh","mi","mk","ml","mn","mr","ms","mt","my","na","nb","nd","ne","ng","nl","nn","no","nr","nv","ny","oc","oj","om","or","os","pa","pi","pl","ps","pt","qu","rm","rn","ro","ru","rw","sa","sc","sd","se","sg","si","sk","sl","sm","sn","so","sq","sr","ss","st","su","sv","sw","ta","te","tg","th","ti","tk","tl","tn","to","tr","ts","tt","tw","ty","ug","uk","ur","uz","vi","vo","wa","wo","xh","yi","yo","za","zh","zu"]
 
-current_date = (2021, 4, 20) # this needs to be synchronised with erroneous results from datefinder
+current_date = (2021, 4, 20) # this needs to be synchronised with erroneous results from datefinder. Format: (yyyy, mm, dd), e.g., (2021, 4, 20).
 
 pattern_brackets = re.compile(r'\[[^\]]*\]') # https://stackoverflow.com/questions/640001/how-can-i-remove-text-within-parentheses-with-a-regex
 pattern_brackets2 = re.compile(r'\([^\)]*\)') # https://stackoverflow.com/questions/640001/how-can-i-remove-text-within-parentheses-with-a-regex
